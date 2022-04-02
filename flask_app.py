7f971cb8257cfeaf622719a96343926885a25d91
Patrick Pragman <pat@pragman.io>
2022-04-01 19:53:05 -0800

    playing with git hub

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+import git
+from flask import Flask, request
 
 app = Flask(__name__)
 
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('pragman_dot_io')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 
 @app.route('/')
