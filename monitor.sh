c7dd142c284cae0e48d6cd22737c1ee63765a472
Patrick Pragman <pat@pragman.io>
2022-04-15 20:53:07 -0800

    how about that

---
+++ monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# much thank to stack overflow for this one - not exactly sexy, but should work for my purposes.
+
+changed=0
+git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
+if [ $changed = 1 ]; then
+    git pull
+    echo "Updated successfully";
+else
+    echo "Up-to-date"
+fi
