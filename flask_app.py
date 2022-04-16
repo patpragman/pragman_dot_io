a9034c932b2854a169becb8b07aea115e021911f
Patrick Pragman <pat@pragman.io>
2022-04-15 20:06:13 -0800

    that didn't work?

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,7 @@ import git
 from flask import Flask, request
 from os import system
 
-repo = git.Repo('.')
+repo = git.cmd.Git('.')
 
 app = Flask(__name__)
 
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo.pull('origin', 'master')
+        repo.pull()
         os.system('. reload.sh')  # reload
         return 'Website update attempted...', 200
     else:
