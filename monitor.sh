2a50f5ef145969443a11a86b48ed3df2cbb719cc
Patrick Pragman <pat@pragman.io>
2022-04-15 20:54:42 -0800

    how about that

--- monitor.sh
+++ monitor.sh
@@ -5,6 +5,7 @@ changed=0
 git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
 if [ $changed = 1 ]; then
     git pull
+    touch /var/www/test_pragman_io_wsgi.py
     echo "Updated successfully";
 else
     echo "Up-to-date"
