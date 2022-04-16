9320688c8a4f74dd4f3f2def4c78b62fc5337ce2
Patrick Pragman <pat@pragman.io>
2022-04-16 11:02:49 -0800

    reformatting

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,8 @@
-#Metar Bug
-#Patrick Pragman
-#Ciego Services
-#January 31, 2018
-#Flask App to watch for changes in the weather
+# Metar Bug
+# Patrick Pragman
+# Ciego Services
+# January 31, 2018
+# Flask App to watch for changes in the weather
 
 from flask import Flask, render_template, request, jsonify
 from metar import Metar
@@ -13,90 +13,93 @@ from local_config import Path
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+
 @app.route('/marshall')
 def N140KD():
     return render_template('N140KD.html')
+
 
 @app.route('/')
 def index():
     return render_template('main.html')
 
+
 @app.route('/wxwatch')
 def wxwatch():
     return render_template('index.html')
 
+
 @app.route('/brief')
 def brief():
-
-
     metars = get_metars()
     tafs = get_tafs()
 
-
-    stations = ["PALP","PAKU", "PASC", "PAQT", "PAAD", "PAKV","PABR", "PABT"]
+    stations = ["PALP", "PAKU", "PASC", "PAQT", "PAAD", "PAKV", "PABR", "PABT"]
 
     for sta in stations:
         if sta not in metars:
             metars[sta] = {}
-            metars[sta]['raw_text'] = "The station " + sta + " is currently unavailable.  Waiting on NWS data - try refreshing."
+            metars[sta][
+                'raw_text'] = "The station " + sta + " is currently unavailable.  Waiting on NWS data - try refreshing."
         if sta not in tafs:
             tafs[sta] = {}
-            tafs[sta]['raw_text'] = "The station " + sta + " is currently unavailable.  Waiting on NWS data - try refreshing."
+            tafs[sta][
+                'raw_text'] = "The station " + sta + " is currently unavailable.  Waiting on NWS data - try refreshing."
 
-    return render_template('brief.html', metars = metars, tafs = tafs, stations = stations)
+    return render_template('brief.html', metars=metars, tafs=tafs, stations=stations)
+
 
 @app.route('/brief2.py')
 def brief2():
-
-
     metars = get_metars()
     tafs = get_tafs()
 
-
-    stations = ["PALP","PAKU", "PASC", "PAQT", "PAAD", "PAKV","PABR", "PABT"]
+    stations = ["PALP", "PAKU", "PASC", "PAQT", "PAAD", "PAKV", "PABR", "PABT"]
 
     for sta in stations:
         if sta not in metars:
             metars[sta] = {}
-            metars[sta]['raw_text'] = "The station " + sta + " is currently unavailable.  Waiting on NWS data - try refreshing."
+            metars[sta][
+                'raw_text'] = "The station " + sta + " is currently unavailable.  Waiting on NWS data - try refreshing."
         if sta not in tafs:
             tafs[sta] = {}
-            tafs[sta]['raw_text'] = "The station " + sta + " is currently unavailable.  Waiting on NWS data - try refreshing."
+            tafs[sta][
+                'raw_text'] = "The station " + sta + " is currently unavailable.  Waiting on NWS data - try refreshing."
 
-    return render_template('brief2.html', metars = metars, tafs = tafs, stations = stations)
+    return render_template('brief2.html', metars=metars, tafs=tafs, stations=stations)
 
 
 @app.route('/any172')
 def any_one_seventy_two():
     return render_template('generic172.html')
 
+
 @app.route('/get_metar', methods=['POST'])
 def get_report():
     req_data = request.form['code']
 
-
     try:
         raw_report = get_metar(req_data)
         if raw_report[0]:
-            #if the report returns, build it and send it.
+            # if the report returns, build it and send it.
             obs = Metar.Metar(raw_report[1])
 
             lowest = 100000
             try:
                 for skc in obs.sky:
-                    #find the lowest ceiling
+                    # find the lowest ceiling
                     ht = int(skc[1].value())
 
-                    #a ceiling is the lowest broken or overcast layer
+                    # a ceiling is the lowest broken or overcast layer
                     if (skc[0] == "BKN" or skc[0] == "OVC"):
                         if ht < lowest:
                             lowest = skc[1].value()
             except:
-                #if this fails, it's fine, just pass out of here and leave 100k feet as the ceiling
+                # if this fails, it's fine, just pass out of here and leave 100k feet as the ceiling
                 pass
 
-            #validate some of the things I want to send back
-            #python gets angry if you don't make sure there is data to send
+            # validate some of the things I want to send back
+            # python gets angry if you don't make sure there is data to send
             try:
                 vis = obs.vis.value()
             except:
@@ -117,26 +120,26 @@ def get_report():
             return jsonify(response)
 
         if not raw_report[0]:
-            #if the report is unavailable, tell the user
-            return jsonify({"ERROR" : True,
-                        "ICAO" : req_data,
-                        "ERROR_TYPE": raw_report[1]})
+            # if the report is unavailable, tell the user
+            return jsonify({"ERROR": True,
+                            "ICAO": req_data,
+                            "ERROR_TYPE": raw_report[1]})
     except:
-        #in the event that this process fails, we need to still send something back to the frontend
-        #we'll call all of these "server errors" - at some point I should probably log these or something
-        #I'm not sure what the best course of action for this would be.
-        return jsonify({"ERROR" : True,
-                        "ICAO" : req_data,
+        # in the event that this process fails, we need to still send something back to the frontend
+        # we'll call all of these "server errors" - at some point I should probably log these or something
+        # I'm not sure what the best course of action for this would be.
+        return jsonify({"ERROR": True,
+                        "ICAO": req_data,
                         "ERROR_TYPE": "SERVER ERROR"})
 
 
-
-@app.route('/get_stations',methods = ['GET'])
+@app.route('/get_stations', methods=['GET'])
 def get_stations():
-    #open up the stations list and send it as requested
+    # open up the stations list and send it as requested
     with open(Path.stations, "r") as station_list:
         data = station_list.read()
         return data
+
 
 if __name__ == "__main__":
     app.run()
