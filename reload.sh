24d3fe7ed6d3fbad6e3e3c0c79d27191f2bf9305
Patrick Pragman <pat@pragman.io>
2022-04-15 23:24:41 -0800

    how about that

--- reload.sh
+++ reload.sh
@@ -2,8 +2,8 @@
 
 # fetch changes
 
-git pull
-git merge master/origin
+git reset --hard
+git pull origin/master
 
 echo "reloading web application, GIT PUSH RECEIVED!"
 touch /var/www/test_pragman_io_wsgi.py
