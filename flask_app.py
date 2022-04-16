262c3b8ccc3827727f023cf712ede299334c9dc4
Patrick Pragman <pat@pragman.io>
2022-04-15 23:44:53 -0800

    curious what this will do

--- flask_app.py
+++ flask_app.py
@@ -10,6 +10,11 @@ app = Flask(__name__)
 def home():  # put application's code here
     return 'Changed pipeline - not entirely autonomous...but yah...'
 
+@app.route('/check_in_am')
+def check():
+    return "Let's see if this worked..."
+
+
 
 if __name__ == '__main__':
     app.run()
