7a4d4c32f78a1fc4502b702b445e91c8387964a0
Patrick Pragman <pat@pragman.io>
2022-04-20 22:08:44 -0800

    adding nbody physics, WIP

--- flask_app.py
+++ flask_app.py
@@ -36,12 +36,12 @@ def brief():
     for sta in stations:
         if sta not in metars:
             metars[sta] = {}
-            metars[sta][
-                'raw_text'] = "The station " + sta + " is currently unavailable.  Waiting on NWS data - try refreshing."
+            metars[sta]['raw_text'] = \
+                f"The station {sta} is currently unavailable.  Waiting on NWS data - try refreshing."
         if sta not in tafs:
             tafs[sta] = {}
-            tafs[sta][
-                'raw_text'] = "The station " + sta + " is currently unavailable.  Waiting on NWS data - try refreshing."
+            tafs[sta]['raw_text'] = \
+                "The station {sta} is currently unavailable.  Waiting on NWS data - try refreshing."
 
     return render_template('brief.html', metars=metars, tafs=tafs, stations=stations)
 
@@ -89,18 +89,14 @@ def get_report():
             obs = Metar.Metar(raw_report[1])
 
             lowest = 100000
-            try:
-                for skc in obs.sky:
-                    # find the lowest ceiling
-                    ht = int(skc[1].value())
+            for skc in obs.sky:
+                # find the lowest ceiling
+                ht = int(skc[1].value())
 
-                    # a ceiling is the lowest broken or overcast layer
-                    if (skc[0] == "BKN" or skc[0] == "OVC"):
-                        if ht < lowest:
-                            lowest = skc[1].value()
-            except:
-                # if this fails, it's fine, just pass out of here and leave 100k feet as the ceiling
-                pass
+                # a ceiling is the lowest broken or overcast layer
+                if (skc[0] == "BKN" or skc[0] == "OVC"):
+                    if ht < lowest:
+                        lowest = skc[1].value()
 
             # validate some of the things I want to send back
             # python gets angry if you don't make sure there is data to send
---
+++ static/js/physics.js
@@ -0,0 +1,84 @@
+// Physics Gravity simulator!
+// Pat Pragman
+// Ciego Services
+// 20APR22
+
+// global variables for drawing everything
+var SCREEN = document.getElementById('screen');
+var CONTEXT = SCREEN.getContext("2d");
+
+// global variable for tick rate
+const dt = 0.000001
+
+class Vector {
+    constructor(x=0, y=0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    vector_add(other_vector:Vector) {
+        this.x = this.x + other_vector.x;
+        this.y = this.y + other_vector.y;
+    }
+
+    multiply_by_number(real_number:number){
+        this.x *= real_number
+        this.y *= real_number
+    }
+
+
+}
+
+class Object {
+    constructor(r:Vector=Vector(0,0),
+                v:Vector=Vector(0, 0),
+                a:Vector=Vector(0,0),
+                m=1){
+        this.r = r;
+        this.v = v;
+        this.a = a;
+        this.m = m;
+    }
+
+    update() {
+        // scale the acceleration, then add that to the velocity vector
+        var delta_v = this.a.multiply_by_number(dt)
+        this.v = this.v.vector_add(delta_v)
+        var delta_r = this.v.multiply_by_number(dt)
+        this.r = this.r.vector_add(delta_r)
+    }
+
+    draw() {
+        // draws circle of radius m at the vector r
+        CONTEXT.beginPath();
+        CONTEXT.arc(
+            this.r.x,
+            this.r.y,
+            this.m,
+            0, 2 * Math.PI,
+            false)
+        CONTEXT.stroke()
+    }
+
+}
+
+function draw(objects){
+    CONTEXT.globalCompositeOperation = 'desination-over';
+    // clear everything
+    CONTEXT.clearRect(0, 0, SCREEN.width, SCREEN.height);
+    CONTEXT.save()
+
+    for (let i = 0; i < length(objects); i++) {
+        o = objects[i]
+        o.update()
+        o.draw()
+        CONTEXT.save()
+    }
+
+}
+
+o = Object()
+o.v = Vector(1, 1)
+var objects = [o]
+
+window.requestAnimationFrame(draw)
