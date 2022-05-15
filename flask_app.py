ccbe7971e3a01bd42c65e1f0a7634fe1916dc531
Patrick Pragman <pat@pragman.io>
2022-05-14 16:55:46 -0800

    deleted function I don't want.

--- flask_app.py
+++ flask_app.py
@@ -25,9 +25,7 @@ def index():
 def wxwatch():
     return render_template('wxwatcher.html')
 
-@app.route('/nbody.html')
-def nbody():
-    return render_template('nbody.html')
+
 
 @app.route('/brief')
 def brief():
